c13310c07c1e46260cbc2ba5138c71105814623f
Messitos16 <149245706+Messitos16@users.noreply.github.com>
2024-01-11 11:23:52 +0300

    Add files via upload

---
+++ 2/script.js
@@ -0,0 +1,6 @@
+let values = [100, 55, 15, 300, 20];
+let sumResult = 0;
+for (let i = 0; i < values.length; i++) {
+    sumResult += values[i];
+}
+console.log("Сумма значений массива:", sumResult);
